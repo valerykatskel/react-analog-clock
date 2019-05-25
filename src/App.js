1dd9455aef33ea6757b37ecd8d4053ea0abfc71e
handlar <ne.vse.srazu@gmail.com>
2019-05-25 14:55:21 +0300

    first commit for react analog clock

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Редактируйте <code>src/App.js</code> и сохраняйте для получения результата.
         </p>
         <a
           className="App-link"
