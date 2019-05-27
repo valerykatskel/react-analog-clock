984f5812de7f6c1dc610089f1e51e91405276e5d
handlar <ne.vse.srazu@gmail.com>
2019-05-27 23:00:25 +0300

    final version before heroku deploy

---
+++ build/precache-manifest.1f7f1aeff94326b08deed7a912caea0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac70d3f6778608ccb452dbf6bf7a32a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "998fabd36d6794834fd8",
+    "url": "/static/js/main.2335c1d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
